341fb30255617d79735b6ad02f7aa5fcd246192b
Vincent <56333156+vinelim@users.noreply.github.com>
2020-05-14 16:00:30 +0800

    updating app with a shop page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-const Homepage = () => (
+const Homepage = ({history}) => (
     <div className="homepage">
         <Directory />
     </div>
